675c75095a32a4f1a1016dbfa3b64c3d9a053c7a
sunbreak1211 <129470872+sunbreak1211@users.noreply.github.com>
2024-04-04 08:57:20 -0300

    Minor text changes
    Co-authored-by: oldchili <130549691+oldchili@users.noreply.github.com>

--- src/LockstakeClipper.sol
+++ src/LockstakeClipper.sol
@@ -53,7 +53,7 @@ interface LockstakeEngineLike {
     function onRemove(address, uint256, uint256) external;
 }
 
-// Clipper for use with the manager / proxy paradigm
+// Clipper for use with the Lockstake Engine
 contract LockstakeClipper {
     // --- Auth ---
     mapping (address => uint256) public wards;
@@ -88,7 +88,7 @@ contract LockstakeClipper {
         uint256 pos;  // Index in active array
         uint256 tab;  // Dai to raise       [rad]
         uint256 lot;  // collateral to sell [wad]
-        uint256 tot;  // static registry of tot collateral to sell [wad]
+        uint256 tot;  // static registry of total collateral to sell [wad]
         address usr;  // Liquidated CDP
         uint96  tic;  // Auction start time
         uint256 top;  // Starting price     [ray]
@@ -264,7 +264,7 @@ contract LockstakeClipper {
             vat.suck(vow, kpr, coin);
         }
 
-        // Trigger proxy manager liquidation call-back
+        // Trigger engine liquidation call-back
         engine.onKick(usr, lot);
 
         emit Kick(id, top, tab, lot, usr, kpr, coin);

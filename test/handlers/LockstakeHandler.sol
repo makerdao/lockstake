b113b9273c6bdef2015006baf337cab9f5562eed
oldchili <130549691+oldchili@users.noreply.github.com>
2024-04-11 09:56:24 +0300

    More bindings

--- test/handlers/LockstakeHandler.sol
+++ test/handlers/LockstakeHandler.sol
@@ -50,6 +50,7 @@ contract LockstakeHandler is DssTest {
     address   public  currentFarm;
     uint256   public  currentAuctionId;
     address   public  yankCaller;
+    uint256   public  mkrNgtRate;
 
     mapping(bytes32 => uint256) public numCalls;
 
@@ -107,6 +108,7 @@ contract LockstakeHandler is DssTest {
         sender = sender_;
         urn    = urn_;
         yankCaller = yankCaller_;
+        mkrNgtRate = engine.mkrNgtRate();
 
         vat.hope(address(clip));
 
@@ -119,6 +121,13 @@ contract LockstakeHandler is DssTest {
             farms.push(farms_[i]);
         }
         farms.push(address(0));
+    }
+
+    function _divup(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        // Note: _divup(0,0) will return 0 differing from natural solidity division
+        unchecked {
+            z = x != 0 ? ((x - 1) / y) + 1 : 0;
+        }
     }
 
     function sumDelegated() external view returns (uint256 sum) {
@@ -180,19 +189,27 @@ contract LockstakeHandler is DssTest {
 
     function free(address to, uint256 wad) external useSender() {
         numCalls["free"]++;
+
+        (uint256 ink,) = vat.urns(ilk, urn);
+        wad = bound(wad, 0, ink);
+
         engine.free(urn, to, wad);
     }
 
     function freeNgt(address to, uint256 ngtWad) external useSender() {
         numCalls["freeNgt"]++;
+
+        (uint256 ink,) = vat.urns(ilk, urn);
+        ngtWad = bound(ngtWad, 0, ink * mkrNgtRate);
+
         engine.freeNgt(urn, to, ngtWad);
     }
 
     function draw(uint256 wad) external useSender() {
         numCalls["draw"]++;
+
         (uint256 ink,) = vat.urns(ilk, urn);
         (,, uint256 spotPrice,, uint256 dust) = vat.ilks(ilk);
-
         wad = bound(wad, dust / RAY, ink * spotPrice / RAY);
 
         engine.draw(urn, address(this), wad);
@@ -200,6 +217,11 @@ contract LockstakeHandler is DssTest {
 
     function wipe(uint256 wad) external useSender() {
         numCalls["wipe"]++;
+
+        (, uint256 art) = vat.urns(ilk, urn);
+        (, uint256 rate,,,) = vat.ilks(ilk);
+        wad = bound(wad, 0, _divup(art * rate, RAY));
+
         deal(address(nst), sender, wad);
         nst.approve(address(engine), wad);
 

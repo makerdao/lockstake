e967ab20cfb68ce1840a2344f69b63288054b0d3
oldchili <130549691+oldchili@users.noreply.github.com>
2024-04-26 17:59:07 +0300

    Invariant Testing (#11)
    * Stake with lock, withdraw with free
    
    * clean farm onTakeLeftovers
    
    * Add missing farm reset in onTakeLeftovers
    
    * Reset farm and delegate on each onKick and onTakeLeftovers
    
    * Fixes on _selectFarm
    
    * Minor move of call
    
    * Fix issue in onKick + reorder some functions
    
    * First round of adapting tests for new structure
    
    * Fix for onTakeLeftovers
    
    * Invariant testing - wip
    
    * Add more invariants
    
    * Add kicking
    
    * Add take
    
    * Fix wipe, add invariant_call_summary
    
    * Add yank support
    
    * Minor changes
    
    * Align to dev, add test/handlers/LockstakeHandler.sol
    
    * Minor changes
    
    * Simplify
    
    * Align to dev, add defaults foundry.toml
    
    * With foundry.toml
    
    * Read vote delegate stake instead of balance
    
    * Apply suggestions from code review
    
    Co-authored-by: sunbreak1211 <129470872+sunbreak1211@users.noreply.github.com>
    
    * More bindings
    
    * Update test/handlers/LockstakeHandler.sol
    
    Co-authored-by: telome <130504305+telome@users.noreply.github.com>
    
    * Fix to use handler.delegatedTo and not rely on balance
    
    * assert => assertTrue
    
    * Remove incorrect function inherited by the Handler (#42)
    
    * Remove excludeArtifact
    
    * Bind free and freeNgt by ink minus debt
    
    * Apply suggestions from code review
    
    use spot in upper bound calcs
    
    Co-authored-by: telome <130504305+telome@users.noreply.github.com>
    
    * wipe from handler
    
    Co-authored-by: telome <130504305+telome@users.noreply.github.com>
    
    * lock from handler
    
    * Fixes and changes for invariant tests (#45)
    
    * Fixes and changes for invariant tests
    
    * Use wad
    
    * Fix typo and renaming
    
    * Simplify structure
    
    * Changes to draw and wipe
    
    * Uncomment lines
    
    * revert on free to engine
    
    ---------
    
    Co-authored-by: oldchili <130549691+oldchili@users.noreply.github.com>
    
    * Add warp
    
    * Update test/handlers/LockstakeHandler.sol
    
    Co-authored-by: sunbreak1211 <129470872+sunbreak1211@users.noreply.github.com>
    
    * Lower duty
    
    * Fix for draw and wipe
    
    ---------
    
    Co-authored-by: sunbreak <sunbreak1211@proton.me>
    Co-authored-by: sunbreak1211 <129470872+sunbreak1211@users.noreply.github.com>
    Co-authored-by: telome <130504305+telome@users.noreply.github.com>

---
+++ test/LockstakeEngine-invariants.t.sol
@@ -0,0 +1,283 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity ^0.8.16;
+
+import "dss-test/DssTest.sol";
+import { LockstakeEngine } from "src/LockstakeEngine.sol";
+import { LockstakeClipper } from "src/LockstakeClipper.sol";
+import { LockstakeMkr } from "src/LockstakeMkr.sol";
+import { PipMock } from "test/mocks/PipMock.sol";
+import { VoteDelegateFactoryMock, VoteDelegateMock } from "test/mocks/VoteDelegateMock.sol";
+import { GemMock } from "test/mocks/GemMock.sol";
+import { NstMock } from "test/mocks/NstMock.sol";
+import { NstJoinMock } from "test/mocks/NstJoinMock.sol";
+import { StakingRewardsMock } from "test/mocks/StakingRewardsMock.sol";
+import { MkrNgtMock } from "test/mocks/MkrNgtMock.sol";
+import { LockstakeHandler } from "test/handlers/LockstakeHandler.sol";
+
+interface ChainlogLike {
+    function getAddress(bytes32) external view returns (address);
+}
+
+interface VatLike {
+    function gem(bytes32, address) external view returns (uint256);
+    function urns(bytes32, address) external view returns (uint256, uint256);
+    function rely(address) external;
+    function file(bytes32, bytes32, uint256) external;
+    function file(bytes32, uint256) external;
+    function init(bytes32) external;
+}
+
+interface SpotterLike {
+    function file(bytes32, bytes32, address) external;
+    function file(bytes32, bytes32, uint256) external;
+    function poke(bytes32) external;
+}
+
+interface JugLike {
+    function file(bytes32, bytes32, uint256) external;
+    function init(bytes32) external;
+}
+
+interface DogLike {
+    function rely(address) external;
+    function file(bytes32, bytes32, address) external;
+    function file(bytes32, bytes32, uint256) external;
+}
+
+interface CalcFabLike {
+    function newStairstepExponentialDecrease(address) external returns (address);
+}
+
+interface CalcLike {
+    function file(bytes32, uint256) external;
+}
+
+contract LockstakeEngineIntegrationTest is DssTest {
+    using stdStorage for StdStorage;
+
+    address             public pauseProxy;
+    VatLike             public vat;
+    address             public spot;
+    DogLike             public dog;
+    GemMock             public mkr;
+    address             public jug;
+    LockstakeEngine     public engine;
+    address             public urn;
+    LockstakeClipper    public clip;
+    PipMock             public pip;
+    VoteDelegateFactoryMock public delFactory;
+    NstMock             public nst;
+    NstJoinMock         public nstJoin;
+    LockstakeMkr        public lsmkr;
+    GemMock             public rTok;
+    StakingRewardsMock  public farm0;
+    StakingRewardsMock  public farm1;
+    MkrNgtMock          public mkrNgt;
+    GemMock             public ngt;
+    bytes32             public ilk = "LSE";
+    address             public voter0;
+    address             public voter1;
+    address             public voterDelegate0;
+    address             public voterDelegate1;
+
+    LockstakeHandler    public handler;
+
+    address constant LOG = 0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F;
+
+    function _divup(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        unchecked {
+            z = x != 0 ? ((x - 1) / y) + 1 : 0;
+        }
+    }
+
+    function ray(uint256 wad) internal pure returns (uint256) {
+        return wad * 10 ** 9;
+    }
+
+    function rad(uint256 wad) internal pure returns (uint256) {
+        return wad * 10 ** 27;
+    }
+
+    function setUp() public {
+        vm.createSelectFork(vm.envString("ETH_RPC_URL"));
+
+        pauseProxy = ChainlogLike(LOG).getAddress("MCD_PAUSE_PROXY");
+        vat = VatLike(ChainlogLike(LOG).getAddress("MCD_VAT"));
+        spot = ChainlogLike(LOG).getAddress("MCD_SPOT");
+        dog = DogLike(ChainlogLike(LOG).getAddress("MCD_DOG"));
+        mkr = new GemMock(0);
+        jug = ChainlogLike(LOG).getAddress("MCD_JUG");
+        nst = new NstMock();
+        nstJoin = new NstJoinMock(address(vat), address(nst));
+        lsmkr = new LockstakeMkr();
+        rTok = new GemMock(0);
+        farm0 = new StakingRewardsMock(address(rTok), address(lsmkr));
+        farm1 = new StakingRewardsMock(address(rTok), address(lsmkr));
+        ngt = new GemMock(0);
+        mkrNgt = new MkrNgtMock(address(mkr), address(ngt), 25_000);
+
+        pip = new PipMock();
+        delFactory = new VoteDelegateFactoryMock(address(mkr));
+        voter0 = address(123);
+        voter1 = address(456);
+        vm.prank(voter0); voterDelegate0 = delFactory.create();
+        vm.prank(voter1); voterDelegate1 = delFactory.create();
+
+        vm.startPrank(pauseProxy);
+        engine = new LockstakeEngine(address(delFactory), address(nstJoin), ilk, address(mkrNgt), address(lsmkr), 15 * WAD / 100);
+        engine.file("jug", jug);
+        vat.rely(address(engine));
+        vat.init(ilk);
+        JugLike(jug).init(ilk);
+        JugLike(jug).file(ilk, "duty", 1000000021979553151239153027); // 100% APY
+        SpotterLike(spot).file(ilk, "pip", address(pip));
+        SpotterLike(spot).file(ilk, "mat", 3 * 10**27); // 300% coll ratio
+        pip.setPrice(1000 * 10**18); // 1 MKR = 1000 USD
+        SpotterLike(spot).poke(ilk);
+        vat.file(ilk, "dust", rad(20 ether)); // $20 dust
+
+        vat.file(ilk, "line", type(uint256).max);
+        vat.file("Line", type(uint256).max);
+
+        // dog and clip setup
+        dog.file(ilk, "chop", 1.1 ether); // 10% chop
+        dog.file(ilk, "hole", rad(1_000_000 ether));
+
+        // dust and chop filed previously so clip.chost will be set correctly
+        clip = new LockstakeClipper(address(vat), spot, address(dog), address(engine));
+        clip.upchost();
+        clip.rely(address(dog));
+
+        dog.file(ilk, "clip", address(clip));
+        dog.rely(address(clip));
+        vat.rely(address(clip));
+        engine.rely(address(clip));
+
+        // setup for take
+        address calc = CalcFabLike(ChainlogLike(LOG).getAddress("CALC_FAB")).newStairstepExponentialDecrease(pauseProxy);
+        CalcLike(calc).file("cut",  RAY - ray(0.01 ether));  // 1% decrease
+        CalcLike(calc).file("step", 1);                      // Decrease every 1 second
+
+        clip.file("buf",  ray(1.25 ether));   // 25% Initial price buffer
+        clip.file("calc", address(calc));     // File price contract
+        clip.file("cusp", ray(0.3 ether));    // 70% drop before reset
+        clip.file("tail", 3600);              // 1 hour before reset
+        vm.stopPrank();
+
+        lsmkr.rely(address(engine));
+
+        deal(address(mkr), address(this), 100_000 * 10**18, true);
+        deal(address(ngt), address(this), 100_000 * 25_000 * 10**18, true);
+
+        // Add some existing DAI assigned to nstJoin to avoid a particular error
+        stdstore.target(address(vat)).sig("dai(address)").with_key(address(nstJoin)).depth(0).checked_write(100_000 * RAD);
+
+        address[] memory voteDelegates = new address[](2);
+        voteDelegates[0] = voterDelegate0;
+        voteDelegates[1] = voterDelegate1;
+
+        address[] memory farms = new address[](2);
+        farms[0] = address(farm0);
+        farms[1] = address(farm1);
+
+        urn = engine.open(0);
+        handler = new LockstakeHandler(
+            vm,
+            address(engine),
+            urn,
+            address(spot),
+            address(dog),
+            pauseProxy,
+            voteDelegates,
+            farms
+        );
+
+        // uncomment and fill to only call specific functions
+//      bytes4[] memory selectors = new bytes4[](5);
+//      selectors[0] = LockstakeHandler.lock.selector;
+//      selectors[1] = LockstakeHandler.draw.selector;
+//      selectors[2] = LockstakeHandler.selectVoteDelegate.selector;
+//      selectors[3] = LockstakeHandler.dropPriceAndBark.selector;
+//      selectors[4] = LockstakeHandler.yank.selector;
+//
+//      targetSelector(FuzzSelector({
+//          addr: address(handler),
+//          selectors: selectors
+//      }));
+
+        targetContract(address(handler)); // invariant tests should fuzz only handler functions
+    }
+
+    function invariant_system_mkr_equals_ink() public {
+        (uint256 ink,) = vat.urns(ilk, urn);
+        assertEq(mkr.balanceOf(address(engine)) + handler.sumDelegated() - vat.gem(ilk, address(clip)) - vat.gem(ilk, pauseProxy), ink);
+    }
+
+    function invariant_system_mkr_equals_lsmkr_total_supply() public {
+        assertEq(mkr.balanceOf(address(engine)) + handler.sumDelegated() - vat.gem(ilk, address(clip)) - vat.gem(ilk, pauseProxy), lsmkr.totalSupply());
+    }
+
+    function invariant_delegation_exclusiveness() public {
+        assertLe(handler.numDelegated(), 1);
+    }
+
+    function invariant_delegation_all_or_nothing() public {
+        address urnDelegate = engine.urnVoteDelegates(urn);
+        (uint256 ink,) = vat.urns(ilk, urn);
+
+        if (urnDelegate == address(0)) {
+            assertEq(mkr.balanceOf(address(engine)) - vat.gem(ilk, address(clip)) - vat.gem(ilk, pauseProxy), ink);
+        } else {
+            assertEq(mkr.balanceOf(address(engine)) - vat.gem(ilk, address(clip)) - vat.gem(ilk, pauseProxy), 0);
+            assertEq(handler.delegatedTo(urnDelegate), ink);
+        }
+    }
+
+    function invariant_staking_exclusiveness() public {
+        assertLe(handler.numStakedForUrn(handler.urn()), 1);
+    }
+
+    function invariant_staking_all_or_nothing() public {
+        address urnFarm = engine.urnFarms(urn);
+        (uint256 ink,) = vat.urns(ilk, urn);
+
+        if (urnFarm == address(0)) {
+            assertEq(lsmkr.balanceOf(urn), ink);
+        } else {
+            assertEq(lsmkr.balanceOf(urn), 0);
+            assertEq(GemMock(urnFarm).balanceOf(urn), ink);
+        }
+    }
+
+    function invariant_no_delegation_or_staking_during_auction() public {
+        assertTrue(
+            engine.urnAuctions(urn) == 0 ||
+            engine.urnVoteDelegates(urn) == address(0) && engine.urnFarms(urn) == address(0)
+        );
+    }
+
+    function invariant_call_summary() private view { // make external to enable
+        console.log("------------------");
+
+        console.log("\nCall Summary\n");
+        console.log("addFarm", handler.numCalls("addFarm"));
+        console.log("selectFarm", handler.numCalls("selectFarm"));
+        console.log("selectVoteDelegate", handler.numCalls("selectVoteDelegate"));
+        console.log("lock", handler.numCalls("lock"));
+        console.log("lockNgt", handler.numCalls("lockNgt"));
+        console.log("free", handler.numCalls("free"));
+        console.log("freeNgt", handler.numCalls("freeNgt"));
+        console.log("draw", handler.numCalls("draw"));
+        console.log("wipe", handler.numCalls("wipe"));
+        console.log("dropPriceAndBark", handler.numCalls("dropPriceAndBark"));
+        console.log("take", handler.numCalls("take"));
+        console.log("yank", handler.numCalls("yank"));
+        console.log("warp", handler.numCalls("warp"));
+        console.log("total count", handler.numCalls("addFarm") + handler.numCalls("selectFarm") + handler.numCalls("selectVoteDelegate") +
+                                   handler.numCalls("lock") + handler.numCalls("lockNgt") + handler.numCalls("free") +
+                                   handler.numCalls("freeNgt") + handler.numCalls("draw") + handler.numCalls("wipe") +
+                                   handler.numCalls("dropPriceAndBark") + handler.numCalls("take") + handler.numCalls("yank") +
+                                   handler.numCalls("warp"));
+    }
+}
---
+++ test/handlers/LockstakeHandler.sol
@@ -0,0 +1,351 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.16;
+
+import "dss-test/DssTest.sol";
+
+import { LockstakeEngine } from "src/LockstakeEngine.sol";
+import { LockstakeClipper } from "src/LockstakeClipper.sol";
+import { GemMock } from "test/mocks/GemMock.sol";
+import { PipMock } from "test/mocks/PipMock.sol";
+
+interface VatLike {
+    function urns(bytes32, address) external view returns (uint256, uint256);
+    function ilks(bytes32) external view returns (uint256, uint256, uint256, uint256, uint256);
+    function hope(address) external;
+    function suck(address, address, uint256) external;
+}
+
+interface JugLike {
+    function ilks(bytes32) external view returns (uint256, uint256);
+}
+
+interface SpotterLike {
+    function ilks(bytes32) external view returns (address, uint256);
+    function poke(bytes32) external;
+}
+
+interface VoteDelegateLike {
+    function stake(address) external view returns (uint256);
+}
+
+interface DogLike {
+    function bark(bytes32, address, address) external returns (uint256);
+    function ilks(bytes32) external view returns (address, uint256, uint256, uint256);
+}
+
+contract LockstakeHandler is StdUtils, StdCheats {
+    Vm vm;
+
+    LockstakeEngine  public engine;
+    GemMock          public mkr;
+    GemMock          public ngt;
+    GemMock          public nst;
+    bytes32          public ilk;
+    VatLike          public vat;
+    JugLike          public jug;
+    SpotterLike      public spot;
+    DogLike          public dog;
+    LockstakeClipper public clip;
+
+    address   public pauseProxy;
+    address   public urn;
+    address   public urnOwner;
+    address[] public voteDelegates;
+    address[] public farms;
+    uint256   public mkrNgtRate;
+    address   public anyone = address(123);
+
+    mapping(bytes32 => uint256) public numCalls;
+
+    uint256 constant RAY = 10 ** 27;
+
+    modifier callAsAnyone() {
+        vm.startPrank(anyone);
+        _;
+        vm.stopPrank();
+    }
+
+    modifier callAsUrnOwner() {
+        vm.startPrank(urnOwner);
+        _;
+        vm.stopPrank();
+    }
+
+    modifier callAsPauseProxy() {
+        vm.startPrank(pauseProxy);
+        _;
+        vm.stopPrank();
+    }
+
+    constructor(
+        Vm vm_,
+        address engine_,
+        address urn_,
+        address spot_,
+        address dog_,
+        address pauseProxy_,
+        address[] memory voteDelegates_,
+        address[] memory farms_
+    ) {
+        vm         = vm_;
+        engine     = LockstakeEngine(engine_);
+        mkr        = GemMock(address(engine.mkr()));
+        ngt        = GemMock(address(engine.ngt()));
+        nst        = GemMock(address(engine.nst()));
+        pauseProxy = pauseProxy_;
+        ilk        = engine.ilk();
+        vat        = VatLike(address(engine.vat()));
+        jug        = JugLike(address(engine.jug()));
+        spot       = SpotterLike(spot_);
+        dog        = DogLike(dog_);
+
+        (address clip_, , , ) = dog.ilks(ilk);
+        clip       = LockstakeClipper(clip_);
+        urn        = urn_;
+        urnOwner   = engine.urnOwners(urn);
+        mkrNgtRate = engine.mkrNgtRate();
+
+        vat.hope(address(clip));
+
+        for (uint256 i = 0; i < voteDelegates_.length ; i++) {
+            voteDelegates.push(voteDelegates_[i]);
+        }
+        voteDelegates.push(address(0));
+
+        for (uint256 i = 0; i < farms_.length ; i++) {
+            farms.push(farms_[i]);
+        }
+        farms.push(address(0));
+    }
+
+    function _rpow(uint256 x, uint256 n, uint256 b) internal pure returns (uint256 z) {
+        assembly {
+            switch x case 0 {switch n case 0 {z := b} default {z := 0}}
+            default {
+                switch mod(n, 2) case 0 { z := b } default { z := x }
+                let half := div(b, 2)  // for rounding.
+                for { n := div(n, 2) } n { n := div(n,2) } {
+                    let xx := mul(x, x)
+                    if iszero(eq(div(xx, x), x)) { revert(0,0) }
+                    let xxRound := add(xx, half)
+                    if lt(xxRound, xx) { revert(0,0) }
+                    x := div(xxRound, b)
+                    if mod(n,2) {
+                        let zx := mul(z, x)
+                        if and(iszero(iszero(x)), iszero(eq(div(zx, x), z))) { revert(0,0) }
+                        let zxRound := add(zx, half)
+                        if lt(zxRound, zx) { revert(0,0) }
+                        z := div(zxRound, b)
+                    }
+                }
+            }
+        }
+    }
+
+    function _divup(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        // Note: _divup(0,0) will return 0 differing from natural solidity division
+        unchecked {
+            z = x != 0 ? ((x - 1) / y) + 1 : 0;
+        }
+    }
+
+    function _min(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        z = x < y ? x : y;
+    }
+
+    function _getRandomVoteDelegate(uint256 voteDelegateIndex) internal view returns (address) {
+        return voteDelegates[bound(voteDelegateIndex, 0, voteDelegates.length - 1)];
+    }
+
+    function _getRandomFarm(uint256 farmIndex) internal view returns (address) {
+        return farms[bound(farmIndex, 0, farms.length - 1)];
+    }
+
+    function _getRandomAuctionId(uint256 auctionIndex) internal view returns (uint256) {
+        uint256[] memory active = clip.list();
+        return active[bound(auctionIndex, 0, active.length - 1)];
+    }
+
+    function delegatedTo(address voteDelegate) external view returns (uint256) {
+        return VoteDelegateLike(voteDelegate).stake(address(engine));
+    }
+
+    function sumDelegated() external view returns (uint256 sum) {
+        for (uint256 i = 0; i < voteDelegates.length; i++) {
+            if (voteDelegates[i] == address(0)) continue;
+            sum += VoteDelegateLike(voteDelegates[i]).stake(address(engine));
+        }
+    }
+
+    // note: There is no way to get the amount delegated per urn from the actual unmodified vote delegate contract,
+    //       so we currently just return the total num of voteDelegates that anyone delegated to.
+    //       In practice it means that invariant_delegation_exclusiveness can only work when there is one urn.
+    function numDelegated() external view returns (uint256 num) {
+        for (uint256 i = 0; i < voteDelegates.length; i++) {
+            if (voteDelegates[i] == address(0)) continue;
+            if (VoteDelegateLike(voteDelegates[i]).stake(address(engine)) > 0) num++;
+        }
+    }
+
+    function numStakedForUrn(address urn_) external view returns (uint256 num) {
+        for (uint256 i = 0; i < farms.length; i++) {
+            if (farms[i] == address(0)) continue;
+            if (GemMock(farms[i]).balanceOf(urn_) > 0) num++;
+        }
+    }
+
+    function addFarm(uint256 farmIndex) callAsPauseProxy() external {
+        numCalls["addFarm"]++;
+        engine.addFarm(_getRandomFarm(farmIndex));
+    }
+
+    function selectFarm(uint16 ref, uint256 farmIndex) callAsUrnOwner() external {
+        numCalls["selectFarm"]++;
+        engine.selectFarm(urn, _getRandomFarm(farmIndex), ref);
+    }
+
+    function selectVoteDelegate(uint256 voteDelegateIndex) callAsUrnOwner() external {
+        numCalls["selectVoteDelegate"]++;
+        engine.selectVoteDelegate(urn, _getRandomVoteDelegate(voteDelegateIndex));
+    }
+
+    function lock(uint256 wad, uint16 ref) external callAsAnyone {
+        numCalls["lock"]++;
+
+        // wad = bound(wad, 0, uint256(type(int256).max) / 10**18) * 10**18;
+        (uint256 ink,) = vat.urns(ilk, urn);
+        (,, uint256 spotPrice,,) = vat.ilks(ilk);
+        wad = bound(wad, 0, _min(
+                                uint256(type(int256).max),
+                                type(uint256).max / spotPrice - ink
+                            ) / 10**18
+                    ) * 10**18;
+
+        deal(address(mkr), anyone, wad);
+        mkr.approve(address(engine), wad);
+
+        engine.lock(urn, wad, ref);
+    }
+
+    function lockNgt(uint256 ngtWad, uint16 ref) external callAsAnyone {
+        numCalls["lockNgt"]++;
+
+        // ngtWad = bound(ngtWad, 0, uint256(type(int256).max) / 10**18) * 10**18;
+        (uint256 ink,) = vat.urns(ilk, urn);
+        (,, uint256 spotPrice,,) = vat.ilks(ilk);
+        ngtWad = bound(ngtWad, 0, _min(
+                                    uint256(type(int256).max),
+                                    _min(
+                                        type(uint256).max / spotPrice - ink,
+                                        type(uint256).max / mkrNgtRate
+                                    )
+                                ) / 10**18
+                      ) * 10**18 * mkrNgtRate;
+
+        deal(address(ngt), anyone, ngtWad);
+        ngt.approve(address(engine), ngtWad);
+
+        engine.lockNgt(urn, ngtWad, ref);
+    }
+
+    function free(address to, uint256 wad) external callAsUrnOwner() {
+        numCalls["free"]++;
+
+        if (to == address(engine)) { revert("free-to-engine-unsupported"); }
+
+        (uint256 ink, uint256 art) = vat.urns(ilk, urn);
+        (, uint256 rate, uint256 spotPrice,,) = vat.ilks(ilk);
+        wad = bound(wad, 0, ink - _divup(art * rate, spotPrice));
+
+        engine.free(urn, to, wad);
+    }
+
+    function freeNgt(address to, uint256 ngtWad) external callAsUrnOwner() {
+        numCalls["freeNgt"]++;
+
+        (uint256 ink, uint256 art ) = vat.urns(ilk, urn);
+        (, uint256 rate, uint256 spotPrice,,) = vat.ilks(ilk);
+        ngtWad = bound(ngtWad, 0, (ink - _divup(art * rate, spotPrice)) * mkrNgtRate);
+
+        engine.freeNgt(urn, to, ngtWad);
+    }
+
+    function draw(uint256 wad) external callAsUrnOwner() {
+        numCalls["draw"]++;
+
+        (uint256 ink, uint256 art) = vat.urns(ilk, urn);
+        (uint256 Art, uint256 rate, uint256 spotPrice,, uint256 dust) = vat.ilks(ilk);
+        (uint256 duty, uint256 rho) = jug.ilks(ilk);
+        rate = _rpow(duty, block.timestamp - rho, RAY) * rate / RAY;
+        if (ink * spotPrice < art * rate) { revert("unsafe-fee-accumulation-or-price-drop"); }
+        wad = bound(wad, art > 0 ? 0 : dust / RAY, _min(
+                                                        (ink * spotPrice - art * rate) / RAY,
+                                                        _min(
+                                                            uint256(type(int256).max) / RAY,
+                                                            (type(uint256).max - Art) * rate / RAY
+                                                        )
+                                                    ));
+
+        engine.draw(urn, address(this), wad);
+    }
+
+    function wipe(uint256 wad) external callAsAnyone {
+        numCalls["wipe"]++;
+
+        (, uint256 art) = vat.urns(ilk, urn);
+        (, uint256 rate,,, uint256 dust) = vat.ilks(ilk);
+        wad = bound(wad, 0, art > 0 ? _min(
+                                            (art * rate - dust) / RAY ,
+                                            uint256(type(int256).max) / RAY
+                                        )
+                                    : 0);
+
+        deal(address(nst), anyone, wad);
+        nst.approve(address(engine), wad);
+
+        engine.wipe(urn, wad);
+    }
+
+    function dropPriceAndBark() external {
+        numCalls["dropPriceAndBark"]++;
+        (uint256 ink, uint256 art) = vat.urns(ilk, urn);
+        (address pip, uint256 mat) = spot.ilks(ilk);
+        (, uint256 rate,,,) = vat.ilks(ilk);
+
+        uint256 minCollateralizedPrice = ((art * rate / RAY) * mat / ink) / 10**9;
+        PipMock(pip).setPrice(minCollateralizedPrice - 1);
+        spot.poke(ilk);
+
+        dog.bark(ilk, urn, address(0));
+    }
+
+    function take(uint256 auctionIndex) external {
+        numCalls["take"]++;
+        LockstakeClipper.Sale memory sale;
+        uint256 auctionId = _getRandomAuctionId(auctionIndex);
+        (sale.pos, sale.tab, sale.lot, sale.tot, sale.usr, sale.tic, sale.top) = clip.sales(auctionId);
+
+        vm.startPrank(pauseProxy); // we use startPrank as cannot override an ongoing prank with a single vm.prank
+        vat.suck(address(0), address(this), sale.tab);
+        vm.stopPrank();
+
+        clip.take({
+            id:  auctionId,
+            amt: sale.lot,
+            max: type(uint256).max,
+            who: address(this),
+            data: ""
+        });
+    }
+
+    function yank(uint256 auctionIndex) external callAsPauseProxy() {
+        numCalls["yank"]++;
+        clip.yank(_getRandomAuctionId(auctionIndex));
+    }
+
+    function warp(uint256 secs) external {
+        numCalls["warp"]++;
+        secs = bound(secs, 0, 365 days);
+        vm.warp(block.timestamp + secs);
+    }
+}

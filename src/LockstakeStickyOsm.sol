fc1688ea2ab7322e89c01d577b0da6419fd32582
sunbreak1211 <sunbreak1211@proton.me>
2025-10-02 14:32:55 -0300

    Add LockstakeStickyOsm

---
+++ src/LockstakeStickyOsm.sol
@@ -0,0 +1,249 @@
+// SPDX-FileCopyrightText: © 2025 Dai Foundation <www.daifoundation.org>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity ^0.8.21;
+
+interface OracleLike {
+    function peek() external view returns (uint256, bool);
+}
+
+contract LockstakeStickyOsm {
+    // --- storage variables ---
+
+    mapping(address usr => uint256 allowed)     public wards;
+    mapping(address usr => uint256 whitelisted) public bud;
+    mapping(address usr => uint256 capped)      public capped;
+
+    uint256 public stopped;
+    uint16  public hop = 1 hours;
+    uint64  public zzz;
+    uint256 public cap;     // [wad]
+    uint256 public alpha;   // Between 0 and 1 [wad]
+    uint256 public top;     // [wad]
+    uint256 public ewma;    // [wad]
+
+    struct Feed {
+        uint128 val; // [wad]
+        uint128 has;
+    }
+
+    Feed fCur; // Free Current Price
+    Feed fNxt; // Free Next Price
+    Feed lCur; // Limited Current Price
+    Feed lNxt; // Limited Next Price
+
+    // --- immutables ---
+
+    OracleLike public immutable  src;
+
+    // --- constants ---
+
+    uint256 public constant WAD = 10**18;
+
+    // --- events ---   
+
+    event Rely(address indexed usr);
+    event Deny(address indexed usr);
+    event Kiss(address indexed usr);
+    event Diss(address indexed usr);
+    event Lock(address indexed usr);
+    event Free(address indexed usr);
+    event File(bytes32 indexed what, uint256 data);
+    event LogValue(bytes32 val);
+
+    // --- modifiers ---
+
+    modifier auth {
+        require(wards[msg.sender] == 1, "LockstakeStickyOsm/not-authorized");
+        _;
+    }
+
+    modifier toll {
+        require(bud[msg.sender] == 1, "LockstakeStickyOsm/contract-not-whitelisted");
+        _;
+    }
+
+    modifier stoppable {
+        require(stopped == 0, "LockstakeStickyOsm/is-stopped");
+        _;
+    }
+
+    // --- constructor ---
+
+    constructor(address src_, uint256 ewma_) {
+        src = OracleLike(src_);
+        ewma = ewma_;
+
+        wards[msg.sender] = 1;
+        emit Rely(msg.sender);
+    }
+
+    // --- internals ---
+
+    function _min(uint256 w, uint256 x, uint256 y) internal pure returns (uint256 z) {
+        uint256 a = w < x ? w : x;
+        z = a < y ? a : y;
+    }
+
+    // --- administration ---
+
+    function rely(address usr) external auth {
+        wards[usr] = 1;
+        emit Rely(usr);
+    }
+
+    function deny(address usr) external auth {
+        wards[usr] = 0;
+        emit Deny(usr);
+    }
+
+    function kiss(address usr) external auth {
+        bud[usr] = 1;
+        emit Kiss(usr);
+    }
+
+    function diss(address usr) external auth {
+        bud[usr] = 0;
+        emit Diss(usr);
+    }
+
+    // function kiss(address[] calldata a) external auth {
+    //     for(uint256 i = 0; i < a.length; i++) {
+    //         require(a[i] != address(0), "LockstakeStickyOsm/no-contract-0");
+    //         bud[a[i]] = 1;
+    //     }
+    // }
+
+    // function diss(address[] calldata a) external auth {
+    //     for(uint256 i = 0; i < a.length; i++) {
+    //         bud[a[i]] = 0;
+    //     }
+    // }
+
+    function lock(address usr) external auth {
+        capped[usr] = 1;
+        emit Lock(usr);
+    }
+
+    function free(address usr) external auth {
+        capped[usr] = 0;
+        emit Free(usr);
+    }
+
+    function file(bytes32 what, uint256 data) external auth {
+        if (what == "cap") {
+            cap = data;
+        } else if (what == "alpha") {
+            require (data <= WAD);
+            alpha = data;
+        } else if (what == "top") {
+            top = data;
+        } else revert("LockstakeStickyOsm/file-unrecognized-param");
+        emit File(what, data);
+    }
+
+    function stop() external auth {
+        stopped = 1;
+    }
+
+    function start() external auth {
+        stopped = 0;
+    }
+
+    function _prev(uint256 ts) internal view returns (uint64) {
+        require(hop != 0, "LockstakeStickyOsm/hop-is-zero");
+        return uint64(ts - ts % hop);
+    }
+
+    function step(uint16 ts) external auth {
+        require(ts > 0, "LockstakeStickyOsm/ts-is-zero");
+        hop = ts;
+    }
+
+    function void() external auth {
+        fCur = fNxt = lCur = lNxt = Feed(0, 0);
+        stopped = 1;
+    }
+
+    function pass() public view returns (bool ok) {
+        return block.timestamp >= zzz + hop;
+    }
+
+    // --- setter ---
+
+    function poke() external stoppable {
+        require(pass(), "LockstakeStickyOsm/not-passed");
+        (uint256 wut, bool ok) = OracleLike(src).peek();
+        require(wut <= type(uint128).max, "LockstakeStickyOsm/overflow");
+        if (ok) {
+            fCur = fNxt;
+            fNxt = Feed(uint128(wut), 1);
+            lCur = lNxt;
+            ewma = (alpha * wut + (WAD - alpha) * ewma) / WAD;
+            lNxt = Feed(uint128(_min(wut, ewma * top / WAD, cap)), 1);
+            zzz = _prev(block.timestamp);
+            emit LogValue(bytes32(uint256(fCur.val)));
+        }
+    }
+
+    function peek() external view toll returns (uint256, bool) {
+        return capped[msg.sender] == 1
+               ? (lCur.val, lCur.has == 1)
+               : (fCur.val, fCur.has == 1);
+    }
+
+    function peep() external view toll returns (uint256, bool) {
+        return capped[msg.sender] == 1
+               ? (lNxt.val, lNxt.has == 1)
+               : (fNxt.val, fNxt.has == 1);
+    }
+
+    function read() external view toll returns (uint256) {
+        if (capped[msg.sender] == 1) {
+            require(lCur.has == 1, "LockstakeStickyOsm/no-current-value");
+            return lCur.val;
+        } else {
+            require(fCur.has == 1, "LockstakeStickyOsm/no-current-value");
+            return fCur.val;
+        }
+    }
+
+    function fPeek() external view toll returns (uint256, bool) {
+        return (fCur.val, fCur.has == 1);
+    }
+
+    function fPeep() external view toll returns (uint256, bool) {
+        return (fNxt.val, fNxt.has == 1);
+    }
+
+    function fRead() external view toll returns (uint256) {
+        require(fCur.has == 1, "LockstakeStickyOsm/no-current-value");
+        return fCur.val;
+    }
+
+    function lPeek() external view toll returns (uint256, bool) {
+        return (lCur.val, lCur.has == 1);
+    }
+
+    function lPeep() external view toll returns (uint256, bool) {
+        return (lNxt.val, lNxt.has == 1);
+    }
+
+    function lRead() external view toll returns (uint256) {
+        require(lCur.has == 1, "LockstakeStickyOsm/no-current-value");
+        return lCur.val;
+    }
+}
---
+++ test/LockstakeStickyOsm.t.sol
@@ -0,0 +1,339 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity ^0.8.21;
+
+import "dss-test/DssTest.sol";
+
+import { LockstakeStickyOsm } from "src/LockstakeStickyOsm.sol";
+import { PipMock } from "test/mocks/PipMock.sol";
+
+contract LockstakeStickyOsmTest is DssTest {
+
+    PipMock feed;
+    LockstakeStickyOsm osm;
+    address cappedUsr = address(111);
+
+    event Kiss(address indexed usr);
+    event Diss(address indexed usr);
+    event Lock(address indexed usr);
+    event Free(address indexed usr);
+    event LogValue(bytes32 val);
+
+    function setUp() public {
+        vm.warp(block.timestamp - block.timestamp % 1 hours); // Start from top of the hour
+        feed = new PipMock();
+        feed.setPrice(100e18);
+        osm = new LockstakeStickyOsm(address(feed), 100e18);
+        osm.file("cap", 1_000e18);
+        osm.file("alpha", 0.1e18);
+        osm.file("top", 1.05e18);
+        osm.step(1 hours);
+        vm.warp(block.timestamp + 1 hours);
+        osm.poke();
+        osm.kiss(address(this));
+        osm.kiss(cappedUsr);
+        osm.lock(cappedUsr);
+    }
+
+    function testAuth() public {
+        checkAuth(address(osm), "LockstakeStickyOsm");
+    }
+
+    function testFile() public {
+        checkFileUint(address(osm), "LockstakeStickyOsm", ["cap", "alpha", "top"]);
+    }
+
+    function testModifiers() public {
+        bytes4[] memory authedMethods = new bytes4[](8);
+        authedMethods[0] = osm.kiss.selector;
+        authedMethods[1] = osm.diss.selector;
+        authedMethods[2] = osm.lock.selector;
+        authedMethods[3] = osm.free.selector;
+        authedMethods[4] = osm.stop.selector;
+        authedMethods[5] = osm.start.selector;
+        authedMethods[6] = osm.step.selector;
+        authedMethods[7] = osm.void.selector;
+
+        // this checks the case where sender is not authed
+        vm.startPrank(address(0xBEEF));
+        checkModifier(address(osm), "LockstakeStickyOsm/not-authorized", authedMethods);
+        vm.stopPrank();
+
+        bytes4[] memory budMethods = new bytes4[](9);
+        budMethods[0] = osm.peek.selector;
+        budMethods[1] = osm.read.selector;
+        budMethods[2] = osm.peep.selector;
+        budMethods[3] = osm.fPeek.selector;
+        budMethods[4] = osm.fRead.selector;
+        budMethods[5] = osm.fPeep.selector;
+        budMethods[6] = osm.lPeek.selector;
+        budMethods[7] = osm.lRead.selector;
+        budMethods[8] = osm.lPeep.selector;
+
+        // this checks the case where sender is not a bud
+        vm.startPrank(address(0xBEEF));
+        checkModifier(address(osm), "LockstakeStickyOsm/contract-not-whitelisted", budMethods);
+        vm.stopPrank();
+    }
+
+    function testKissDiss() public {
+        assertEq(osm.bud(address(123)), 0);
+        vm.expectEmit();
+        emit Kiss(address(123));
+        osm.kiss(address(123));
+        assertEq(osm.bud(address(123)), 1);
+        vm.expectEmit();
+        emit Diss(address(123));
+        osm.diss(address(123));
+        assertEq(osm.bud(address(123)), 0);
+    }
+
+    function testLockFree() public {
+        assertEq(osm.capped(address(123)), 0);
+        vm.expectEmit();
+        emit Lock(address(123));
+        osm.lock(address(123));
+        assertEq(osm.capped(address(123)), 1);
+        vm.expectEmit();
+        emit Free(address(123));
+        osm.free(address(123));
+        assertEq(osm.capped(address(123)), 0);
+    }
+
+    function testStep() public {
+        assertEq(osm.hop(), 3600);
+        osm.step(7200);
+        assertEq(osm.hop(), 7200);
+        vm.expectRevert("LockstakeStickyOsm/ts-is-zero");
+        osm.step(0);
+    }
+
+    function testStopStart() public {
+        assertEq(osm.stopped(), 0);
+        osm.stop();
+        assertEq(osm.stopped(), 1);
+        osm.start();
+        assertEq(osm.stopped(), 0);
+    }
+
+    function testVoid() public {
+        assertEq(osm.stopped(), 0);
+        vm.warp(block.timestamp + 1 hours);
+        osm.poke();
+        (uint256 val, bool has) = osm.fPeek();
+        assertEq(val, 100e18);
+        assertTrue(has);
+        (val, has) = osm.fPeep();
+        assertEq(val, 100e18);
+        assertTrue(has);
+        (val, has) = osm.lPeek();
+        assertEq(val, 100e18);
+        assertTrue(has);
+        (val, has) = osm.lPeep();
+        assertEq(val, 100e18);
+        assertTrue(has);
+
+        osm.void();
+
+        assertEq(osm.stopped(), 1);
+        (val, has) = osm.fPeek();
+        assertEq(val, 0);
+        assertTrue(!has);
+        (val, has) = osm.fPeep();
+        assertEq(val, 0);
+        assertTrue(!has);
+        (val, has) = osm.lPeek();
+        assertEq(val, 0);
+        assertTrue(!has);
+        (val, has) = osm.lPeep();
+        assertEq(val, 0);
+        assertTrue(!has);
+    }
+
+    function testPoke() public {
+        feed.setPrice(101e18);
+
+        vm.warp(block.timestamp + 1 hours);
+        osm.poke();
+
+        (uint256 val, bool has) = osm.fPeek();
+        assertEq(val, 100e18);
+        assertTrue(has);
+
+        (val, has) = osm.fPeep();
+        assertEq(val, 101e18);
+        assertTrue(has);
+
+        (val, has) = osm.lPeek();
+        assertEq(val, 100e18);
+        assertTrue(has);
+
+        (val, has) = osm.lPeep();
+        assertEq(val, 101e18);
+        assertTrue(has);
+
+        vm.warp(block.timestamp + 1 hours);
+        osm.poke();
+
+        (val, has) = osm.fPeek();
+        assertEq(val, 101e18);
+        assertTrue(has);
+
+        (val, has) = osm.lPeek();
+        assertEq(val, 101e18);
+        assertTrue(has);
+
+        vm.warp(block.timestamp + 1 hours - 1);
+        vm.expectRevert("LockstakeStickyOsm/not-passed");
+        osm.poke();
+
+        vm.warp(block.timestamp + 1);
+        feed.setPrice(uint256(type(uint128).max) + 1);
+        vm.expectRevert("LockstakeStickyOsm/overflow");
+        osm.poke();
+
+        osm.stop();
+        vm.expectRevert("LockstakeStickyOsm/is-stopped");
+        osm.poke();
+    }
+
+    function testPokeEWMATopLimit() public {
+        feed.setPrice(110e18);
+        assertEq(osm.ewma(), 100e18); // Initial ewma value
+
+        vm.warp(block.timestamp + 1 hours);
+        osm.poke();
+
+        assertEq(osm.ewma(), (0.1e18 * 110e18 + 0.9e18 * 100e18) / 1e18); // 101e18
+        assertEq(osm.ewma() * osm.top() / 1e18, 106.05e18); // ewma + top that will be exposed below as the limited price
+
+        (uint256 val, bool has) = osm.fPeek();
+        assertEq(val, 100e18);
+        assertTrue(has);
+
+        (val, has) = osm.fPeep();
+        assertEq(val, 110e18);
+        assertTrue(has);
+
+        assertEq(osm.fRead(), 100e18);
+
+        (val, has) = osm.lPeek();
+        assertEq(val, 100e18);
+        assertTrue(has);
+
+        (val, has) = osm.lPeep();
+        assertEq(val, 106.05e18);
+        assertTrue(has);
+
+        assertEq(osm.lRead(), 100e18);
+
+        (val, has) = osm.peek();
+        assertEq(val, 100e18);
+        assertTrue(has);
+
+        (val, has) = osm.peep();
+        assertEq(val, 110e18);
+        assertTrue(has);
+
+        assertEq(osm.read(), 100e18);
+
+        vm.prank(cappedUsr); (val, has) = osm.peek();
+        assertEq(val, 100e18);
+        assertTrue(has);
+
+        vm.prank(cappedUsr); (val, has) = osm.peep();
+        assertEq(val, 106.05e18);
+        assertTrue(has);
+
+        vm.prank(cappedUsr); assertEq(osm.read(), 100e18);
+
+        vm.warp(block.timestamp + 1 hours);
+        osm.poke();
+
+        assertEq(osm.ewma(), (0.1e18 * 110e18 + 0.9e18 * 101e18) / 1e18); // 101.9e18
+        assertEq(osm.ewma() * osm.top() / 1e18, 106.995e18);
+
+        (val, has) = osm.fPeek();
+        assertEq(val, 110e18);
+        assertTrue(has);
+
+        (val, has) = osm.fPeep();
+        assertEq(val, 110e18);
+        assertTrue(has);
+
+        assertEq(osm.fRead(), 110e18);
+
+        (val, has) = osm.lPeek();
+        assertEq(val, 106.05e18);
+        assertTrue(has);
+
+        (val, has) = osm.lPeep();
+        assertEq(val, 106.995e18);
+        assertTrue(has);
+
+        assertEq(osm.lRead(), 106.05e18);
+
+        (val, has) = osm.peek();
+        assertEq(val, 110e18);
+        assertTrue(has);
+
+        (val, has) = osm.peep();
+        assertEq(val, 110e18);
+        assertTrue(has);
+
+        assertEq(osm.read(), 110e18);
+
+        vm.prank(cappedUsr); (val, has) = osm.peek();
+        assertEq(val, 106.05e18);
+        assertTrue(has);
+
+        vm.prank(cappedUsr); (val, has) = osm.peep();
+        assertEq(val, 106.995e18);
+        assertTrue(has);
+
+        vm.prank(cappedUsr); assertEq(osm.read(), 106.05e18);
+    }
+
+    function testPokeCapLimit() public {
+        feed.setPrice(110e18);
+        osm.file("cap", 105e18);
+
+        vm.warp(block.timestamp + 1 hours);
+        osm.poke();
+
+        assertEq(osm.ewma(), 101e18);
+        assertEq(osm.ewma() * osm.top() / 1e18, 106.05e18); // Both new price and ewma above cap
+
+        (uint256 val, bool has) = osm.fPeek();
+        assertEq(val, 100e18);
+        assertTrue(has);
+
+        (val, has) = osm.fPeep();
+        assertEq(val, 110e18);
+        assertTrue(has);
+
+        (val, has) = osm.lPeek();
+        assertEq(val, 100e18);
+        assertTrue(has);
+
+        (val, has) = osm.lPeep();
+        assertEq(val, 105e18);
+        assertTrue(has);
+
+        (val, has) = osm.peek();
+        assertEq(val, 100e18);
+        assertTrue(has);
+
+        (val, has) = osm.peep();
+        assertEq(val, 110e18);
+        assertTrue(has);
+
+        vm.prank(cappedUsr); (val, has) = osm.peek();
+        assertEq(val, 100e18);
+        assertTrue(has);
+
+        vm.prank(cappedUsr); (val, has) = osm.peep();
+        assertEq(val, 105e18);
+        assertTrue(has);
+    }
+}
